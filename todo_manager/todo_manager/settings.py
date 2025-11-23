551147bd427dc1019aa7dd6c874257609ab81c9c
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-23 15:24:47 +0300

    add templates to urls

--- todo_manager/todo_manager/settings.py
+++ todo_manager/todo_manager/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "todo_manager.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
