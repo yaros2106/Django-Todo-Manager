0a51102cb5ca2bb5eecf3021681fc1e5ba65b864
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-24 16:11:40 +0300

    create ToDoItem model

--- todo_manager/todo_list/models.py
+++ todo_manager/todo_list/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=250)
+    done = models.BooleanField(default=False)
