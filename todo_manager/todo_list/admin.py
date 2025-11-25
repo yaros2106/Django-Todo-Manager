34abfda7c93821362bdb20fd3b8e3d615ffd1f82
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-25 14:08:14 +0300

    connect todo app to admin

--- todo_manager/todo_list/admin.py
+++ todo_manager/todo_list/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_list.models import ToDoItem
+
+
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "done",
+    )
+    list_display_links = (
+        "id",
+        "title",
+    )
--- todo_manager/todo_list/models.py
+++ todo_manager/todo_list/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class ToDoItem(models.Model):
+    class Meta:
+        verbose_name = "ToDo Item"
+
     title = models.CharField(max_length=250)
     done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
