fef897820f4dd9a56e4ae4e805d01f4a3c8fef0f
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-29 15:19:04 +0300

    add description field and get_absolute_url for ToDo item model

--- todo_manager/todo_list/models.py
+++ todo_manager/todo_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ToDoItem(models.Model):
@@ -7,7 +8,14 @@ class ToDoItem(models.Model):
         verbose_name = "ToDo Item"
 
     title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=False)
     done = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse(
+            "todo_list:detail",
+            kwargs={"pk": self.pk},
+        )
 
     def __str__(self) -> str:
         return self.title
