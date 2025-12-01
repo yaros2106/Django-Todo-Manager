dc656594b53360d1a52a4641185c79540d8e05bc
Yaroslav <iaroslav.bierioza@gmail.com>
2025-12-01 20:37:48 +0300

    rename todoitem_list to list

--- todo_manager/todo_list/urls.py
+++ todo_manager/todo_list/urls.py
@@ -12,7 +12,7 @@ from todo_list.views import (
 app_name = "todo_list"
 urlpatterns = [
     path("", ToDoListView.as_view(), name="index"),
-    path("list/", ToDoListIndexView.as_view(), name="todoitem_list"),
+    path("list/", ToDoListIndexView.as_view(), name="list"),
     path("done/", ToDoListDoneView.as_view(), name="done"),
     path("<int:pk>/", ToDoDetailView.as_view(), name="detail"),
     path("<int:pk>/update/", ToDoItemUpdateView.as_view(), name="update"),
